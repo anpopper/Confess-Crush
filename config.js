950b6fb16e628d4d7cdfe3c65b42ead3db1084f9
anpopper <92196503+anpopper@users.noreply.github.com>
2021-10-09 11:43:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "demo",
+    introTitle: 'Cậu ơi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnIntro: 'Hello cậu',
+    title: 'Tớ có điều muốn nói với cậu 🥰',
+    desc: 'tớ yêu cậu, cậu làm người yêu tớ nha ',
+    btnYes: 'yêu vaicalol <33',
+    btnNo: 'cậu bị điên à :))',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    reply: 'vì cậu đẹp try vl á á <33333333',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'Oke lun nhơ mãi yêu <3',
+    messLink: 'https://phucan2002github.com/anpopper/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
